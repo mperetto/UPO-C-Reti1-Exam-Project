73c60e56a368840a493a7fdb4f7d03cef5fbe5b4
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-05-14 18:07:13 +0200

    implementato controllo di sintassi messaggio

--- server.c
+++ server.c
@@ -1,121 +1,191 @@
-#include <stdio.h>      
+#include <stdio.h>
 #include <sys/types.h>
-#include <sys/socket.h>   
+#include <sys/socket.h>
 #include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <time.h>
+#include <ctype.h>
 
 int main(int argc, char const *argv[])
 {
-    int simpleSocket = 0;
-    int simplePort = 0;
-    int returnStatus = 0;
-    struct sockaddr_in simpleServer;
-    char buffer[512];// <Esito> <Tipo> <Contenuto>
+	int simpleSocket = 0;
+	int simplePort = 0;
+	int returnStatus = 0;
+	struct sockaddr_in simpleServer;
+	char buffer[512]; // <Esito> <Tipo> <Contenuto>
 
-    const char ERR_MESSAGES[3][128] = {
-        "ERR SYNTAX Formato messaggio errato, (manca ritorno a capo)\n",
-        "ERR DATA Numero dati fornito non corrisponde con dati trasmessi\n",
-        "ERR STATS Non posso calcolare la varianza"
-    };
+	const char ERR_MESSAGES[3][128] = {
+			"ERR SYNTAX Formato messaggio errato, (manca ritorno a capo)\n",
+			"ERR DATA Numero dati fornito non corrisponde con dati trasmessi\n",
+			"ERR STATS Non posso calcolare la varianza\n"};
 
-    if (2 != argc) {
+	if (2 != argc)
+	{
 
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(1);
+		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
 
-    }
+	simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (simpleSocket == -1)
+	{
 
-    if (simpleSocket == -1) {
+		fprintf(stderr, "Could not create a socket!\n");
+		exit(1);
+	}
+	else
+	{
+		fprintf(stderr, "Socket created!\n");
+	}
 
-        fprintf(stderr, "Could not create a socket!\n");
-        exit(1);
+	/* retrieve the port number for listening */
+	simplePort = atoi(argv[1]);
 
-    }
-    else {
-	    fprintf(stderr, "Socket created!\n");
-    }
+	/* setup the address structure */
+	/* use INADDR_ANY to bind to all local addresses  */
+	memset(&simpleServer, '\0', sizeof(simpleServer));
+	simpleServer.sin_family = AF_INET;
+	simpleServer.sin_addr.s_addr = htonl(INADDR_ANY);
+	simpleServer.sin_port = htons(simplePort);
 
-    /* retrieve the port number for listening */
-    simplePort = atoi(argv[1]);
+	/*  bind to the address and port with our socket  */
+	returnStatus = bind(simpleSocket, (struct sockaddr *)&simpleServer, sizeof(simpleServer));
 
-    /* setup the address structure */
-    /* use INADDR_ANY to bind to all local addresses  */
-    memset(&simpleServer, '\0', sizeof(simpleServer)); 
-    simpleServer.sin_family = AF_INET;
-    simpleServer.sin_addr.s_addr = htonl(INADDR_ANY);
-    simpleServer.sin_port = htons(simplePort);
+	if (returnStatus == 0)
+	{
+		fprintf(stderr, "Bind completed!\n");
+	}
+	else
+	{
+		fprintf(stderr, "Could not bind to address!\n");
+		close(simpleSocket);
+		exit(1);
+	}
 
-    /*  bind to the address and port with our socket  */
-    returnStatus = bind(simpleSocket,(struct sockaddr *)&simpleServer,sizeof(simpleServer));
+	/* lets listen on the socket for connections      */
+	returnStatus = listen(simpleSocket, 5);
 
-    if (returnStatus == 0) {
-	    fprintf(stderr, "Bind completed!\n");
-    }
-    else {
-        fprintf(stderr, "Could not bind to address!\n");
-        close(simpleSocket);
-        exit(1);
-    }
+	if (returnStatus == -1)
+	{
+		fprintf(stderr, "Cannot listen on socket!\n");
+		close(simpleSocket);
+		exit(1);
+	}
 
-    /* lets listen on the socket for connections      */
-    returnStatus = listen(simpleSocket, 5);
+	while (1)
+	{
 
-    if (returnStatus == -1) {
-        fprintf(stderr, "Cannot listen on socket!\n");
-	    close(simpleSocket);
-        exit(1);
-    }
+		struct sockaddr_in clientName = {0};
+		int simpleChildSocket = 0;
+		int clientNameLength = sizeof(clientName);
 
-    while (1){
+		/* wait here */
 
-        struct sockaddr_in clientName = { 0 };
-        int simpleChildSocket = 0;
-        int clientNameLength = sizeof(clientName);
+		simpleChildSocket = accept(simpleSocket, (struct sockaddr *)&clientName, &clientNameLength);
 
-        /* wait here */
+		if (simpleChildSocket == -1)
+		{
 
-        simpleChildSocket = accept(simpleSocket,(struct sockaddr *)&clientName, &clientNameLength);
+			fprintf(stderr, "Cannot accept connections!\n");
+			close(simpleSocket);
+			exit(1);
+		}
 
-        if (simpleChildSocket == -1) {
+		memset(buffer, '\0', sizeof(buffer));
+		strcpy(buffer, "OK START Connessione attiva, Attendo i dati\n");
+		write(simpleChildSocket, buffer, strlen(buffer));
 
-            fprintf(stderr, "Cannot accept connections!\n");
-            close(simpleSocket);
-            exit(1);
+		int esci = 0;//Vale 1 quando si verifica un errore in seguito la conn viene chiusa
+		while (!esci)
+		{
+			memset(buffer, '\0', sizeof(buffer));
+			returnStatus = read(simpleChildSocket, buffer, sizeof(buffer));
 
-        }
+			char *chrFound;
+			chrFound = strrchr(buffer, '\n');
 
-        memset(buffer, '\0', sizeof(buffer));
-        strcpy(buffer, "OK START Connessione attiva, Attendo i dati\n");
-        write(simpleChildSocket, buffer, strlen(buffer));
+			if (chrFound == NULL)
+			{
+				memset(buffer, '\0', sizeof(buffer));
+				strcpy(buffer, ERR_MESSAGES[0]);
+				write(simpleChildSocket, buffer, strlen(buffer));
+				esci = 1;
+			}
+			else
+			{
+				*chrFound = '\0';
 
-        memset(buffer, '\0', sizeof(buffer));
-        returnStatus = read(simpleChildSocket, buffer, sizeof(buffer));
+				//controllo che il messaggio sia sintatticamente corretto
+				//Ogni valore deve essere separato da UNO spazio
+				int i;
+				char chrPrev = 'a';
+				for (i = 0; i < strlen(buffer) && !esci; i++)
+				{
+					if (chrPrev == buffer[i] && buffer[i] == ' ')
+					{
+						memset(buffer, '\0', sizeof(buffer));
+						strcpy(buffer, "ERR SYNTAX Errore formato messaggio errato\n");
+						write(simpleChildSocket, buffer, strlen(buffer));
+						printf("Messaggio sintatticamente Errato, spazi multipli\n");
+						esci = 1;
+					}
+					else if(buffer[i] != ' '){
+						if(!isdigit(buffer[i])){
+							memset(buffer, '\0', sizeof(buffer));
+							strcpy(buffer, "ERR DATA Errore puoi inviare solo numeri\n");
+							write(simpleChildSocket, buffer, strlen(buffer));
+							esci = 1;
+						}
+					}
+					
+					chrPrev = buffer[i];
+				}
 
-        char *chrFound;
-        chrFound = strrchr(buffer, '\n');
+				if (!esci)
+				{
+					char *delim = " ";
+					char *ptr = strtok(buffer, delim);
 
-        if(chrFound == NULL){
-            memset(buffer, '\0', sizeof(buffer));
-            strcpy(buffer, ERR_MESSAGES[0]);
-            write(simpleChildSocket, buffer, strlen(buffer));
-            close(simpleChildSocket);
-        }
-        else{
-            *chrFound = '\0';
-            
-        }
+					if(ptr != NULL){
+						int totValues = atoi(ptr);
+						printf("%s %d\n", ptr, totValues);
+						ptr = strtok(NULL, delim);
 
-        //write(simpleChildSocket, MESSAGE, strlen(MESSAGE));
-        //returnStatus = read(simpleSocket, buffer, sizeof(buffer));
-        close(simpleChildSocket);
+						int ValuesCounter = 0;
+						while(ptr != NULL){
+							ValuesCounter++;
+							printf("Value: %s, Tot: %d\n", ptr, ValuesCounter);
+							ptr = strtok(NULL, delim);
+						}
+						if(ValuesCounter != totValues){
+							if(ValuesCounter < totValues){
+								memset(buffer, '\0', sizeof(buffer));
+								strcpy(buffer, "ERR DATA Errore valori trasmessi inferiori a valori dichiarati\n");
+								write(simpleChildSocket, buffer, strlen(buffer));
+							}
+							else if(ValuesCounter > totValues){
+								memset(buffer, '\0', sizeof(buffer));
+								strcpy(buffer, "ERR DATA Errore valori trasmessi superiori a valori dichiarati\n");
+								write(simpleChildSocket, buffer, strlen(buffer));
+							}
+							esci = 1;
+						}
+						else{
+							memset(buffer, '\0', sizeof(buffer));
+							sprintf(buffer, "OK DATA %d\n", totValues);
+							write(simpleChildSocket, buffer, strlen(buffer));
+						}
+					}
+				}
+			}
+		}
 
-    }
+		close(simpleChildSocket);
+	}
 
-    close(simpleSocket);
-    return 0;
+	close(simpleSocket);
+	return 0;
 }
