e5dbec0716ddd3c7f462a3443f4c5dacc94026ae
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-05-23 21:07:15 +0200

    corretti messaggi di errore + implementato controllo OK START

--- client.c
+++ client.c
@@ -1,66 +1,91 @@
-#include <stdio.h>      
+#include <stdio.h>
 #include <sys/types.h>
-#include <sys/socket.h>   
+#include <sys/socket.h>
 #include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    int simpleSocket = 0;
-    int simplePort = 0;
-    int returnStatus = 0;
-    char buffer[512] = "";
-    struct sockaddr_in simpleServer;
+	int simpleSocket = 0;
+	int simplePort = 0;
+	int returnStatus = 0;
+	char buffer[512] = "";
+	struct sockaddr_in simpleServer;
 
-    if (3 != argc) {
-        fprintf(stderr, "Errore inserimento parametri\n");
-        fprintf(stderr, "Usare: %s <ip_server> <num_porta>\n", argv[0]);
-        exit(1);
+	if (3 != argc)
+	{
+		fprintf(stderr, "Errore inserimento parametri\n");
+		fprintf(stderr, "Usare: %s <ip_server> <num_porta>\n", argv[0]);
+		exit(1);
+	}
 
-    }
+	/* create a streaming socket      */
+	simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    /* create a streaming socket      */
-    simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (simpleSocket == -1)
+	{
+		fprintf(stderr, "Errore non posso creare il Socket\n");
+		exit(1);
+	}
 
-    if (simpleSocket == -1) {
-        fprintf(stderr, "Errore non posso creare il Socket\n");
-        exit(1);
-    }
+	/* retrieve the port number for connecting */
+	simplePort = atoi(argv[2]);
 
-    /* retrieve the port number for connecting */
-    simplePort = atoi(argv[2]);
+	/* setup the address structure */
+	/* use the IP address sent as an argument for the server address  */
+	bzero(&simpleServer, sizeof(simpleServer));
+	simpleServer.sin_family = AF_INET;
+	//inet_addr(argv[2], &simpleServer.sin_addr.s_addr);
+	simpleServer.sin_addr.s_addr = inet_addr(argv[1]);
+	simpleServer.sin_port = htons(simplePort);
 
-    /* setup the address structure */
-    /* use the IP address sent as an argument for the server address  */
-    bzero(&simpleServer, sizeof(simpleServer));
-    simpleServer.sin_family = AF_INET;
-    //inet_addr(argv[2], &simpleServer.sin_addr.s_addr);
-    simpleServer.sin_addr.s_addr=inet_addr(argv[1]);
-    simpleServer.sin_port = htons(simplePort);
+	/*  connect to the address and port with our socket  */
+	returnStatus = connect(simpleSocket, (struct sockaddr *)&simpleServer, sizeof(simpleServer));
 
-    /*  connect to the address and port with our socket  */
-    returnStatus = connect(simpleSocket, (struct sockaddr *)&simpleServer, sizeof(simpleServer));
+	if (returnStatus != 0)
+	{
+		fprintf(stderr, "Impossibile connettersi all'indirizzo\n");
+		close(simpleSocket);
+		exit(1);
+	}
 
-    if (returnStatus != 0) {
-	    fprintf(stderr, "Impossibile connettersi all'indirizzo\n");
-        close(simpleSocket);
-	    exit(1);
-    }
-    
-    /* get the message from the server   */
-    returnStatus = read(simpleSocket, buffer, sizeof(buffer));
+	/* get the message from the server   */
+	memset(buffer, '\0', sizeof(buffer));
+	returnStatus = read(simpleSocket, buffer, sizeof(buffer));
 
-    if ( returnStatus > 0 ) {
-        printf("%d: %s", returnStatus, buffer);
-    } else {
-        fprintf(stderr, "Return Status = %d \n", returnStatus);
-    }
+	char server_msg[512] = "";
 
-    close(simpleSocket);
-    return 0;
+	if (returnStatus > 0)
+	{
+		int esci = 0;
+		{
+			char *str_OK = "OK START ";
+			int i;
+			for(i = 0; i < strlen(str_OK); i++){
+				if(buffer[i] != str_OK[i]){
+					esci = 1;
+					break;
+				}
+			}
+			for(; i < strlen(buffer); i++){
+				char buff_serv[2];
+				buff_serv[1] = '\0';
+				buff_serv[0] = buffer[i];
+				strcat(server_msg, buff_serv);
+			}
+			printf("%sfin\n", server_msg);
+		}
 
+	}
+	else
+	{
+		fprintf(stderr, "Return Status = %d \n", returnStatus);
+	}
+
+	close(simpleSocket);
+	return 0;
 }
-
